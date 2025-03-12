62d313f057059a2c92e56fca956e04c053aab34f
Inês Portugal <ituga02@gmail.com>
2025-03-12 16:10:03 +0000

    Obter colunas e retirar validação no final

--- ipPxbpmV.pl
+++ ipPxbpmV.pl
@@ -1,4 +1,5 @@
-%sudoku(Sudoku,Solution).
+sudoku(Sudoku,Solution):-
+    resolver(Sudoko, Solução).
     /*Teste: sudoku([[5, 3, 0, 0, 7, 8, 0, 0, 0], 
 [6, 0, 2, 1, 9, 5, 3, 4, 8], 
 [1, 9, 0, 3, 4, 0, 5, 6, 7], 
@@ -11,9 +12,20 @@
 Solução).*/ 
 
 %resolver_sudoku(Sudoku):-
- %   validar_linhas(Sudoku),
 
+%Listas das Colunas
+colunas([],[]).
+colunas([L|R],Colunas):-
+    colunas(R, R2),
+    adicionar_numero_coluna(L,R2,Colunas).
 
+adicionar_numero_coluna([],C,C).
+adicionar_numero_coluna([H|T],[],[[H]|R2]):-
+    adicionar_numero_coluna(T,[],R2).
+adicionar_numero_coluna([H|T],[C|Cs],[[H|C]|R2]):-
+    adicionar_numero_coluna(T,Cs,R2).
+
+%Listas das matrizes 3x3
 %Substitui os zeros NAO TESTADO
 substituir_zeros([],[]).
 substituir_zeros([H|T],[H|T2]):- %se não é 0 então nao faz nada e continua
@@ -25,7 +37,7 @@ substituir_zeros([0|T],[N|T2]):-  % se estiver 0
 
 %Verificações
 %
-%Validar se os numeros nao se repetem na linha
+%Validar se ao adicionar numero, este nao se repete na linha
 nao_existe_na_linha(_, []).  
 nao_existe_na_linha(N, [H | T]) :-  
     N \= H,  
@@ -37,9 +49,3 @@ validar_numeros([H|T]):-
     H>=1,
     H=<9,
     validar_numeros(T).
-
-%Validar os numeros das linhas
-validar_linhas([]).
-validar_linhas([L|R]):- %valida linha a linha
-    validar_numeros(L),
-    validar_linhas(R).
