4a805be0037e1352dc25bc7b294fb3db1559495f
Inês Portugal <ituga02@gmail.com>
2025-03-13 00:55:27 +0000

    Update ipPxbpmV.pl
    Falta por as matrizes 3x3 e testar o resto

--- ipPxbpmV.pl
+++ ipPxbpmV.pl
@@ -1,5 +1,6 @@
 sudoku(Sudoku,Solution):-
-    resolver(Sudoko, Solução).
+    resolver(Sudoku),
+    Solution=Sudoku.
     /*Teste: sudoku([[5, 3, 0, 0, 7, 8, 0, 0, 0], 
 [6, 0, 2, 1, 9, 5, 3, 4, 8], 
 [1, 9, 0, 3, 4, 0, 5, 6, 7], 
@@ -11,21 +12,28 @@ sudoku(Sudoku,Solution):-
 [0, 0, 5, 2, 0, 6, 1, 7, 9]],  
 Solução).*/ 
 
-%resolver_sudoku(Sudoku):-
+resolver(Sudoku):-
+    validar_linhas(Sudoku),
+    transpor(Sudoku,Colunas),
+    validar_linhas(Colunas),
+    sub_matrizes(Sudoku,SubMatrizes),
+    validar_linhas(SubMatrizes),
+    
 
-%Listas das Colunas
-colunas([],[]).
-colunas([L|R],Colunas):-
-    colunas(R, R2),
-    adicionar_numero_coluna(L,R2,Colunas).
+%Tranpor a matriz para obter as colunas
+transpor([[]|_],[]).
+transpor(M,[H|T]) :-
+    primeira_coluna(M,H,Mr),
+    transpor(Mr,T).
 
-adicionar_numero_coluna([],C,C).
-adicionar_numero_coluna([H|T],[],[[H]|R2]):-
-    adicionar_numero_coluna(T,[],R2).
-adicionar_numero_coluna([H|T],[C|Cs],[[H|C]|R2]):-
-    adicionar_numero_coluna(T,Cs,R2).
+primeira_coluna([],[],[]).
+primeira_coluna([[X|Xs]|R], [X|C], [Xs|N]) :-
+    primeira_coluna(R, C, N).
 
 %Listas das matrizes 3x3
+sub_matrizes([],[]).
+%sub_matrizes(
+
 %Substitui os zeros NAO TESTADO
 substituir_zeros([],[]).
 substituir_zeros([H|T],[H|T2]):- %se não é 0 então nao faz nada e continua
@@ -36,12 +44,18 @@ substituir_zeros([0|T],[N|T2]):-  % se estiver 0
     substituir_zeros(T,T2).
 
 %Verificações
-%
+
+%Validar as linhas
+validar_linhas([]).
+validar_linhas([H|T]):- %valida linha a linha H=linha e T=linhas restantes
+        nao_tem_repetidos(H),
+        validar_linhas(T).
+        
 %Validar se ao adicionar numero, este nao se repete na linha
-nao_existe_na_linha(_, []).  
-nao_existe_na_linha(N, [H | T]) :-  
-    N \= H,  
-    nao_existe_na_linha(N, T). 
+nao_tem_repetidos([]).  
+nao_tem_repetidos([H | T]) :-  
+    \+ member(H,T), %verifica se H não está no restante da lista 
+    nao_tem_repetidos(T).
 
 %Validar se os numeros estão todos entre 1 e 9
 validar_numeros([]).
